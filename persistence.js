0713573cbaee15790d906f5913cdde2e7ffbc856
MohdBashar <60106447@udst.edu.qa>
2024-03-24 17:36:02 +0300

    worked on retrieving and deleting the session from mongo db  and connected the project to mongodb database.
    Here i have worked on connecting my project to the mongo db database from where we are going to send and retrieve our data. I made several functions named getSessions, deleteSessions and saveSessions.
    I have also removed useraccounts as it was in text and made a json file for the userAccounts data which then i imported to the mongodb database.

--- persistence.js
+++ persistence.js
@@ -1,9 +1,63 @@
 const mongodb = require('mongodb')
 
 let client= undefined
+let db = undefined 
+let session = undefined
+let catloc = undefined
 
 async function connectDatabase(){
     if (!client){
-        client = await  new MongoClient("")
+        client = await  new MongoClient("mongodb+srv://basharsoad:12class34@cluster0.d6lyly6.mongodb.net/")
+        await client.connect()
+        db = client.db("project")
+        session = db.collection("sessionData")
+        catloc = db.collection("catlocation")
+
+        return db
     }
+}
+
+// sd = sessiondata, here we start the session.
+async function startSession(sd){
+    await connectDatabase()
+    let db = client.db("project")
+    let session = db.collection('sessionData')
+    await session.insertOnt(sd)
+}
+
+
+//function to save session.
+async function saveSession(uuid, expiry, data){
+    let db =await connectDatabase()
+    await db[1].replaceOne({SessionKey:uuid, expiry:expiry, data:data})
+}
+
+
+// finding the session based on the session key.
+// The 'getSession' function retrieves a session based on the session key
+
+async function getSession(key){
+    await connectDatabase()
+    let db = client.db('project')
+    let session = db.collection('sessionData')
+    let final_session  = await session.find({sessionNumber:key})
+    let result = await final_session.toArray()
+    return result[0]
+}
+
+//function for deleting the session.
+// The 'deleteSession' function deletes a session from the 'sessionData' collection based on the session key.
+
+async function deleteSession(key){
+    await connectDatabase()
+    db = client.db('project')
+    session = db.collection("sessionData")
+    await session.deleteOne({sessionNumber:key})
+}
+
+module.exports = {
+    startSession,
+    saveSession,
+    getSession,
+    deleteSession,
 }
